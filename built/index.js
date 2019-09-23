ec096e1674a00932512efea94357308b1deb1465
鸡毛 <shihao.gu@perfma.com>
2019-09-23 09:55:14 +0800

    清空文件重新思考接口配置

---
+++ built/index.js
@@ -0,0 +1,7 @@
+"use strict";
+function add(a, b) {
+    return a + b;
+}
+var a = 13;
+var b = 67;
+console.log(add(a, b));
--- src/Utils/DBPool/index.ts
+++
@@ -1,4 +0,0 @@
-import Config from '../../../db.json';
-import bluebird from 'bluebird';
-
-export default Config;
