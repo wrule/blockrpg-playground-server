9b42313719cdb78309131452593be14978a3e653
鸡毛 <shihao.gu@perfma.com>
2019-09-23 15:41:05 +0800

    提交代码

---
+++ src/Entity/Player/Model/index.ts
@@ -0,0 +1,9 @@
+
+export default class Player {
+  private uid: string = '';
+  private name: string = '';
+  private x: number = 0;
+  private y: number = 0;
+  private dir: number = 1;
+  private ges: number = 0;
+}
