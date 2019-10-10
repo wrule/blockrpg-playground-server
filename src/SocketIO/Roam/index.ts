00d9e6d7b7d37df4049bf5373e8512474df4b6a6
鸡毛 <shihao.gu@perfma.com>
2019-10-10 10:38:01 +0800

    定义漫游事件

--- src/SocketIO/Roam/index.ts
+++ src/SocketIO/Roam/index.ts
@@ -8,6 +8,17 @@ export default (io: SocketIO.Server) => {
   const nsp = io.of('/roam').use(AuthCookie);
   // 客户端连接事件
   nsp.on('connection', async (socket) => {
-    
+    // 获取session
+    const sessionCookie = SocketIOCaller.ReadCookie(socket, 'session');
+    if (sessionCookie) {
+      // 获取session
+      const session = (sessionCookie as string).trim();
+      // 根据session获取玩家信息
+      const playerInfo = await SessionBLL.sessionGetAll(session);
+      // 漫游事件，获取漫游数据
+      socket.on('roam', (data) => {
+
+      });
+    }
   });
 }
